6077104329dbf736f04b979162af5f006cade26e
MaxxBahr <schnee.bahr@googlemail.com>
2026-02-10 23:03:35 +0100

    added sorting algorithm

--- src-tauri/src/sorting/mod.rs
+++ src-tauri/src/sorting/mod.rs
@@ -1,10 +1,30 @@
 mod sort{
-    pub fn sort_coffee<T: Params + rusqlite::ToSql>(connection: Arc<Mutex<rusqlite::Connection>>, criteria: T) -> Vec<Coffee>{
-        let unsorted_coffee: Vec<Coffee> = get_entry(connection, "".to_string(), criteria);
-        // Implement quick sort        
+    pub fn sort_coffee(connection: Arc<Mutex<rusqlite::Connection>>) -> Vec<Coffee>{
+        let unsorted_coffee: Vec<Coffee> = get_entry(connection, "".to_string(), "".to_string(), "".to_string());
     }
 
-    fn my_sort<T: PartialEq + Eq + PartialOrd + Ord>(left: Vec<T>, right: Vec<T>) -> Vec<T>{
+    // TODO: sort by ranking or name
+    fn my_sort(arr: &mut Vec<Coffee>, low: i32, high: i32){
+        if low < high{
+            let pi = partition(arr, low, high);
 
+            my_sort(arr, low, pi - 1);
+            my_sort(arr, pi + 1, high);
+        }
+    }
+
+    fn partition(arr: &mut Vec<Coffee>, low: i32, high: i32) -> i32{
+        let pivot = arr[high];
+        let mut i = low - 1;
+
+        for j in low..(high-1){
+            if arr[j] < pivot {
+                i += 1;
+                arr.swap(arr[i], arr[j]);
+            }
+        }
+
+        arr.swap(arr[i+1], arr[high]);
+        return i + 1;
     }
 }
