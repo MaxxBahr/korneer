f738c575a8368fae0327e982fbbbe591b0bc3ae0
MaxxBahr <schnee.bahr@googlemail.com>
2026-02-13 14:48:12 +0100

    finalized sorting algorithm

--- src-tauri/src/sorting/mod.rs
+++ src-tauri/src/sorting/mod.rs
@@ -1,6 +1,8 @@
 mod sort{
     pub fn sort_coffee(connection: Arc<Mutex<rusqlite::Connection>>) -> Vec<Coffee>{
-        let unsorted_coffee: Vec<Coffee> = get_entry(connection, "".to_string(), "".to_string(), "".to_string());
+        let mut unsorted_coffee: Vec<Coffee> = get_entry(connection, "".to_string(), "".to_string(), "".to_string());
+        my_sort(&unsorted_coffee, 0, unsorted_coffee.len());
+        unsorted_coffee
     }
 
     // TODO: sort by ranking or name
@@ -14,17 +16,17 @@ mod sort{
     }
 
     fn partition(arr: &mut Vec<Coffee>, low: i32, high: i32) -> i32{
-        let pivot = arr[high];
+        let pivot = arr[high as usize];
         let mut i = low - 1;
 
-        for j in low..(high-1){
+        for j in low..high{
             if arr[j] < pivot {
                 i += 1;
-                arr.swap(arr[i], arr[j]);
+                arr.swap(i as usize, j as usize);
             }
         }
 
-        arr.swap(arr[i+1], arr[high]);
+        arr.swap((i+1) as usize, high as usize);
         return i + 1;
     }
 }
